87f8656fd6681a3a5084e859385da6bb55a7d7ed
lswgood0901 <lswgood0901@gmail.com>
2021-11-20 21:44:43 +0900

    ongoing

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def entropy(pos):
 
 datasample = pd.read_csv('1_feedback_viewedFP_caadria.csv')
 linkdata = datasample['link']
+fpname_list = datasample['floorplan_name']
+fpname_list = fpname_list.values.tolist()
+event_list = datasample['event']
+event_list = event_list.values.tolist()
 selected_data = []
 
 index = 0
@@ -27,6 +31,12 @@ lclinks = []
 rslinks = []
 rsllinks = []
 rclinks = []
+for i in fpname_list:
+    rest_list = list(filter(lambda x: fpname_list[x] == i, range(len(fpname_list))))
+    if event_list[rest_list[len(rest_list)-1]] == 3:
+        event_list[rest_list[0]] = 1
+    elif event_list[rest_list[len(rest_list)-1]] == 2:
+        event_list[rest_list[0]] = 0
 
 for a in range(len(linkdata)):
     string_to_array = ast.literal_eval(linkdata[a])
@@ -38,20 +48,20 @@ for a in range(len(linkdata)):
     rclink = []
 
     for b in string_to_array:
-        if b[0] == "rn" and datasample['event'][a] != 2:
+        if b[0] == "rn" and (datasample['event'][a] != 2 and datasample['event'][a] != 3):
             rnlink.append([b[1], b[2]])
-        elif b[0] == "fs" and datasample['event'][a] != 2:
+        elif b[0] == "fs" and (datasample['event'][a] != 2 and datasample['event'][a] != 3):
             fslink.append([b[1], b[2]])
-        elif b[0] == "lc" and datasample['event'][a] != 2:
+        elif b[0] == "lc" and (datasample['event'][a] != 2 and datasample['event'][a] != 3):
             lclink.append([b[1], b[2]])
-        elif b[0] == "rs" and datasample['event'][a] != 2:
+        elif b[0] == "rs" and (datasample['event'][a] != 2 and datasample['event'][a] != 3):
             rslink.append([b[1], b[2]])
-        elif b[0] == "rsl" and datasample['event'][a] != 2:
+        elif b[0] == "rsl" and (datasample['event'][a] != 2 and datasample['event'][a] != 3):
             rsllink.append([b[1], b[2]])
-        elif b[0] == "rc" and datasample['event'][a] != 2:
+        elif b[0] == "rc" and (datasample['event'][a] != 2 and datasample['event'][a] != 3):
             rclink.append([b[1], b[2]])
-    if datasample['event'][a] != 2:
-        selected_data.append(datasample['event'][a])
+    if (datasample['event'][a] != 2 and datasample['event'][a] != 3):
+        selected_data.append(event_list[a])
         rnlinks.append(rnlink)
         fslinks.append(fslink)
         lclinks.append(lclink)
@@ -59,8 +69,6 @@ for a in range(len(linkdata)):
         rsllinks.append(rsllink)
         rclinks.append(rclink)
     index += 1
-for i in rnlinks:
-    print(i)
 def appending_currentstate(anylinks):
     index = 0
     current_state = []
@@ -72,7 +80,9 @@ def appending_currentstate(anylinks):
         for link in links:
             nforeLink_pos[link[0]] += 1
             nbackLink_pos[link[1]-1] += 1
-        if( index > 0 ):
+        if( index == 0):
+            current_state.append([0,0])
+        else:
             for i in range(index):
                 nforeLink_entp[i] = entropy(nforeLink_pos[i] / (index - i))
                 nbackLink_entp[i] = entropy(nbackLink_pos[i] / (i + 1))
@@ -87,8 +97,7 @@ lc_cs = appending_currentstate(lclinks)
 rs_cs = appending_currentstate(rslinks)
 rsl_cs = appending_currentstate(rsllinks)
 rc_cs = appending_currentstate(rclinks)
-print(selected_data)
-print(rn_cs)
+
 fore_rncs = []
 back_rncs = []
 fore_fscs = []
@@ -122,7 +131,7 @@ for i in rc_cs:
     back_rccs.append(i[0])
 all_fore = [fore_rncs, fore_fscs, fore_lccs, fore_rscs, fore_rslcs, fore_rccs]
 all_back = [back_rncs, back_fscs, back_lccs, back_rscs, back_rslcs, back_rccs]
-name = ['fore_rncs', 'fore_fscs', 'fore_lccs', 'fore_rscs', 'fore_rslcs', 'fore_rccs', 'back_rncs', 'back_fscs', 'back_lccs', 'back_rscs', 'back_rslcs', 'back_rccs']
+name = ['NR - Div', 'FS - Div', 'LC - Div', 'RS - Div', 'RSL - Div', 'RC - Div', 'NR - Conv', 'FS - Conv', 'LC - Conv', 'RS - Conv', 'RSL - Conv', 'RC - Conv',]
 plt.rc("font", size=8)
 plt.rc('ytick', labelsize=5)
 plt.rc('xtick', labelsize=5)
@@ -134,11 +143,21 @@ index = 0
 for i in all_fore:
     plt.subplot(2,6,index+1)
     plt.plot(x_axis, i)
+    scatter_index = 0
+    for l in i:
+        if selected_data[scatter_index] == 1:
+            plt.scatter(scatter_index, l, s = 10, c = 'r')
+        scatter_index += 1
     plt.title(name[index])
     index += 1
 for i in all_back:
     plt.subplot(2,6,index+1)
     plt.plot(x_axis, i)
+    scatter_index = 0
+    for l in i:
+        if selected_data[scatter_index] == 1:
+            plt.scatter(scatter_index, l, s=10, c='r')
+        scatter_index += 1
     plt.title(name[index])
     index += 1
 plt.show()
